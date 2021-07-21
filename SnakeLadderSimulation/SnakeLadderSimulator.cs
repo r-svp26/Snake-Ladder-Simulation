cef0ac907f25c0fe4a221fffd3d788453af7b54b
r-svp26 <riteshk6341@gmail.com>
2021-07-22 00:17:34 +0530

    [Ritesh] Add:Player reaches exact at hundred position and wins the game

--- SnakeLadderSimulation/SnakeLadderSimulator.cs
+++ SnakeLadderSimulation/SnakeLadderSimulator.cs
@@ -28,9 +28,12 @@ namespace SnakeLadderSimulation
                 die = getDie();
                 if (option == 1)
                 {
-                    playerPosition = playerPosition + die;
-                    Console.WriteLine("Player status is Ladder");
-                    Console.WriteLine("Player position is:" + playerPosition);
+                    if (playerPosition <= 100 && (playerPosition + die) <= 100)
+                    {
+                        playerPosition = playerPosition + die;
+                        Console.WriteLine("Player status is Ladder");
+                        Console.WriteLine("Player position is:" + playerPosition);
+                    }
                 }
                 else if (option == 2)
                 {
