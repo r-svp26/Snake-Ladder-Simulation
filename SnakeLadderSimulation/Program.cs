60fb4d24b350a058bfecf209cc562b43d7544a8e
r-svp26 <riteshk6341@gmail.com>
2021-07-21 17:02:32 +0530

    [Ritesh] Add: display the welcome message

---
+++ SnakeLadderSimulation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeLadderSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake Ladder Simulator.");
+        }
+    }
+}
